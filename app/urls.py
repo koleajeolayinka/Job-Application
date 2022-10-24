3a05b8230b5acc74491cb43b999c3c2e6f068325
KOLEAJEOLAYINKA <koleajeolayinka@gmail.com>
2022-10-24 23:38:54 +0100

    submodule of jobapp project

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,9 @@ from django.urls import path
 from app import views
 
 
-urlpatterns = [path("", views.hello), path("job/<int:id>", views.job_detail), path("job/<str:id>", views.job_detail)]
+urlpatterns = [
+    path('', views.job_list, name="jobs_home"),
+    path('hello/', views.hello, name='hello_display'),
+    path("job/<int:id>", views.job_detail, name="jobs_detail"),
+    # path("job/<str:id>", views.job_detail),
+]
--- app/views.py
+++ app/views.py
@@ -1,30 +1,48 @@
-from cmath import log
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseNotFound
 from django.shortcuts import redirect
+from django.urls import reverse
+from django.template import loader 
 
-job_title = ["Google Department", "Linkedin", "dd"]
-job_description = ["first job description", "second job descrption"]
+job_title = ["first_job", "second_job", "third_job"]
+job_description = [
+    "first job description",
+    "second job descrption",
+    "third job descrption",
+]
 
 
 def hello(request):
+    template = loader.get_template('hello.html')
+    context={}
+    return HttpResponse(template.render(context, request))
+
+def job_list(request):
     # return HttpResponse("Hello world")
     job_id = "http://127.0.0.1:8000/job/0"
 
-    return HttpResponse(
-        f"<ul> <li>{job_title[0]} <li>{job_description[0]}</li></li> <li>{job_title[1]}</li> <li>{job_description[1]}</li> </ul> <a href={job_id}><h1>Job id </h1></a>"
-    )
+    list_of_jobs = "<ul>"
+    for i in job_title:
+        job_id = job_title.index(i)
+        # print(reverse("jobs_detail", args=(job_id,)))
+        detail_url = reverse("jobs_detail", args=(job_id,))
+        list_of_jobs = list_of_jobs + f"<a href='{detail_url}'><li>{i}</li></a>"
+    list_of_jobs = list_of_jobs + "</ul>"
+    return HttpResponse(list_of_jobs)
 
 
 def job_detail(request, id):
-    print(type(id))
     back = "http://127.0.0.1:8000/"
 
-    if id >= 2:
-        return redirect("/")
+    try:
+        if id == 0:
+            return redirect(reverse("jobs_home"))
 
-    return_html = f"<h1>{job_title[int(id)]}</h1> <h3>{job_description[int(id)]}</h3> <a href={back}><h1>back</h1></a>"
+        return_html = f"<h1>{job_title[int(id)]}</h1> <h3>{job_description[int(id)]}</h3> <a href={back}><h1>back</h1></a>"
 
-    return HttpResponse(return_html)
+        return HttpResponse(return_html)
+    except:
+        # return HttpResponse("<h1>Not found</h1>")
+        return HttpResponseNotFound("<h1>Not Found</h1>")
 
     # return HttpResponse(f"Job detail page {id} <h1>header</h1>")
