fa9ac4091d0c460098e1ec7f75c6e97618168350
KOLEAJEOLAYINKA <koleajeolayinka@gmail.com>
2022-11-01 22:39:00 +0100

    submodule of jobapp project

--- app/models.py
+++ app/models.py
@@ -8,13 +8,13 @@ class JobPost(models.Model):
     description = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
     salary = models.IntegerField()
-    slug = models.SlugField(null=True, max_length=40, unique=True)
+    url = models.SlugField(null=True, max_length=40, unique=True)
 
     # auth
 
     def save(self, *args, **kwargs):
         if not self.id:
-            self.slug = slugify(self.title)
+            self.url = slugify(self.title)
         return super(JobPost, self).save(*args, **kwargs)
 
     def __str__(self):
