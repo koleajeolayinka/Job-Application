24bae53846ea19ca0805dd002b11d4a220c1dfb1
KOLEAJEOLAYINKA <koleajeolayinka@gmail.com>
2022-11-10 22:00:15 +0100

    submodule of jobapp project

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from app.models import JobPost
 
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "title", "salary", "date")
+    list_filter = ("salary", "date")
+    search_fields = ("title", "salary")
+    search_help_text = "write in your query and hit enter"
+    # fields= ["title"]
+    # exclude= ("title",)
+    fieldsets = (
+        (
+            "Basic Information", {'fields':("title", "description",)
+        }),
+        ("More Information", 
+        {
+        "classes": ("collapse",),
+
+        "fields":("expire", "salary", "url",)
+        }),
+    )
+
+
 # Register your models here.
-admin.site.register(JobPost)
+admin.site.register(JobPost, JobAdmin)
--- app/models.py
+++ app/models.py
@@ -3,12 +3,31 @@ from django.utils.text import slugify
 
 
 # Create your models here.
+
+class Skills(models.Model):
+    name = models.CharField(max_length=200)
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    designation = models.CharField(max_length=200)
+
+class Location(models.Model):
+    street=models.CharField(max_length=200)
+    city=models.CharField(max_length=200)
+    state=models.CharField(max_length=200)
+    zip=models.CharField(max_length=200)
+
 class JobPost(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
+    expire = models.DateField(null=True)
     salary = models.IntegerField()
     url = models.SlugField(null=True, max_length=40, unique=True)
+    location = models.OneToOneField(Location, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    skills = models.ManyToManyField(Skills)
 
     # auth
 
@@ -18,4 +37,5 @@ class JobPost(models.Model):
         return super(JobPost, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.title
+        return f"{self.title} with {self.salary}"
+
