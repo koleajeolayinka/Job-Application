956a995f4af912524533769562d6efecae0ed433
KOLEAJEOLAYINKA <koleajeolayinka@gmail.com>
2023-03-03 05:18:46 +0100

    43: Enhancement: Enable Debug Mode

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("IS_DEVELOPENT"))
+DEBUG = os.getenv("IS_DEVELOPENT", True)
 
 ALLOWED_HOSTS = [str(os.getenv("APP_HOST"))]
 
